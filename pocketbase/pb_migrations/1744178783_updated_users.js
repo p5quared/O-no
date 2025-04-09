008f32b74fc72a8e03d10c94a27a48152d5b66e8
AriaNas202 <anasca2022@gmail.com>
2025-04-09 16:09:10 -0400

    Updated Database for User Sign Up

---
+++ pocketbase/pb_migrations/1744178783_updated_users.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text4166911607",
+    "max": 0,
+    "min": 0,
+    "name": "username",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(9, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3992264336",
+    "max": 0,
+    "min": 0,
+    "name": "authToken",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove field
+  collection.fields.removeById("text4166911607")
+
+  // remove field
+  collection.fields.removeById("text3992264336")
+
+  return app.save(collection)
+})
