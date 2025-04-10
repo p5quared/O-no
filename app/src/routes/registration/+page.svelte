4770048e070c238d582cb49bd40415a50bd1e0ca
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-09 21:32:01 -0400

    chore: formatting

--- app/src/routes/registration/+page.svelte
+++ app/src/routes/registration/+page.svelte
@@ -1,109 +1,106 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { pb } from '$lib/pb/pocketbase'; 
-
-	
+	import { pb } from '$lib/pb/pocketbase';
 
 	let username = '';
-    let email='';
+	let email = '';
 	let password = '';
-    let passwordConfirm= '';
+	let passwordConfirm = '';
 	let errorMessage = '';
 	let successMessage = '';
 
 	async function register() {
 		errorMessage = '';
 		successMessage = '';
-        
-        if (password != passwordConfirm){
-            errorMessage = "Passwords do not match!";
-            console.error('Passwords Dont Match for User!');
+
+		if (password != passwordConfirm) {
+			errorMessage = 'Passwords do not match!';
+			console.error('Passwords Dont Match for User!');
 			return;
-        }
+		}
 
 		try {
-            //I added new fields to the "users" colleciton
+			// I added new fields to the "users" colleciton
 			await pb.collection('users').create({
 				username,
-                email,
+				email,
 				password,
 				passwordConfirm,
-                authToken: ''
+			authToken: ''
 			});
 			successMessage = 'Account created!';
-            //Im assuming the login route will be /login, but CHANGE THIS if not
+			// Im assuming the login route will be /login, but CHANGE THIS if not
 			setTimeout(() => goto('/login'), 1500);
-            
 		} catch (error: any) {
-            
-            console.error('Something Went Wrong, Account Not Created', error);
+			console.error('Something Went Wrong, Account Not Created', error);
 
-            if(error?.response?.data){
-                let errors=error.response.data
-                if(errors?.username){errorMessage=errorMessage+"Username: "+errors.username.message +" "}
-                if(errors?.password){errorMessage=errorMessage+  "Password: "+errors.password.message +" "}
-                if(errors?.email){errorMessage=errorMessage+  "Email: "+ errors.email.message +" "}
-            }
-            else{
-                errorMessage="Something Went Wrong, No Account Created!"
-            }
+			if (error?.response?.data) {
+				let errors = error.response.data;
+
+				if (errors?.username) {
+					errorMessage = errorMessage + 'Username: ' + errors.username.message + ' ';
+				}
+
+				if (errors?.password) {
+					errorMessage = errorMessage + 'Password: ' + errors.password.message + ' ';
+				}
+
+				if (errors?.email) {
+					errorMessage = errorMessage + 'Email: ' + errors.email.message + ' ';
+				}
+
+			} else {
+				errorMessage = 'Something Went Wrong, No Account Created!';
+			}
 		}
 	}
-
-
-
 </script>
-
-
-
-
-
-
 
 <svelte:head>
 	<title>Registration</title>
 </svelte:head>
 
-
-
-<div class="max-w-md mx-auto p-4">
-	<h1 class="text-2xl font-bold mb-4">Registration</h1>
+<div class="mx-auto max-w-md p-4">
+	<h1 class="mb-4 text-2xl font-bold">Registration</h1>
 
 	<form on:submit|preventDefault={register} class="flex flex-col gap-4">
 		<input
 			type="text"
-            bind:value={username}
+			bind:value={username}
 			placeholder="Username"
 			required
-            autocomplete="off"
-			class="flex-1 p-3 border border-gray-300 rounded"
+			autocomplete="off"
+			class="flex-1 rounded border border-gray-300 p-3"
 		/>
-        <input
+		<input
 			type="email"
-            bind:value={email}
+			bind:value={email}
 			placeholder="Email"
 			required
-            autocomplete="off"
-			class="p-3 border border-gray-300 rounded"
+			autocomplete="off"
+			class="rounded border border-gray-300 p-3"
 		/>
 		<input
 			type="password"
-            bind:value={password}
+			bind:value={password}
 			placeholder="Password"
 			required
-            autocomplete="off"
-			class="p-3 border border-gray-300 rounded"
+			autocomplete="off"
+			class="rounded border border-gray-300 p-3"
 		/>
-        <input
-            type="password"
-            bind:value={passwordConfirm}
-            placeholder="Confirm Password"
-            required
-            autocomplete="off"
-            class="p-3 border border-gray-300 rounded"
-        />
-        <button type="submit" class="px-6 py-3 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded" style= "cursor:pointer">Register</button>
-
+		<input
+			type="password"
+			bind:value={passwordConfirm}
+			placeholder="Confirm Password"
+			required
+			autocomplete="off"
+			class="rounded border border-gray-300 p-3"
+		/>
+		<button
+			type="submit"
+			class="rounded bg-indigo-600 px-6 py-3 font-bold text-white hover:bg-indigo-700"
+			style="cursor:pointer">Register</button
+		>
 
 		{#if errorMessage}
 			<p class="text-red-600">{errorMessage}</p>
@@ -111,7 +108,5 @@
 		{#if successMessage}
 			<p class="text-green-600">{successMessage}</p>
 		{/if}
-
-
 	</form>
 </div>
