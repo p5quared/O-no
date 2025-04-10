2589e9516584ea4e5f54e2b983ad83e3a740e212
Nas <143642674+AriaNas202@users.noreply.github.com>
2025-04-09 21:54:38 -0400

    Registration (#11)
    This is just a basic Registration page. It follows the structure/styling
    of the rest of the website.
    
    ---------
    
    Co-authored-by: Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>

---
+++ app/src/routes/registration/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	export let form: ActionData;
+</script>
+
+<svelte:head>
+	<title>Registration</title>
+</svelte:head>
+
+<div class="mx-auto max-w-md p-4">
+	<h1 class="mb-4 text-2xl font-bold">Registration</h1>
+	{#if form?.error}
+		<p class="text-red-600">{form.error}</p>
+	{/if}
+	<form method="POST" use:enhance class="flex flex-col gap-4">
+		<input
+			type="text"
+			name="username"
+			value={form?.username ?? ''}
+			placeholder="Username"
+			required
+			autocomplete="off"
+			class="flex-1 rounded border border-gray-300 p-3"
+		/>
+		<input
+			type="email"
+			name="email"
+			value={form?.email ?? ''}
+			placeholder="Email"
+			required
+			autocomplete="off"
+			class="rounded border border-gray-300 p-3"
+		/>
+		<input
+			type="password"
+			name="password"
+			placeholder="Password"
+			required
+			autocomplete="off"
+			class="rounded border border-gray-300 p-3"
+		/>
+		<input
+			type="password"
+			name="passwordConfirm"
+			placeholder="Confirm Password"
+			required
+			autocomplete="off"
+			class="rounded border border-gray-300 p-3"
+		/>
+		<button
+			type="submit"
+			class="rounded bg-indigo-600 px-6 py-3 font-bold text-white hover:bg-indigo-700"
+			style="cursor:pointer">Register</button
+		>
+	</form>
+</div>
