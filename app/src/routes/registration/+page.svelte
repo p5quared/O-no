1c0a5a558bb1067c96b8e6f854a3155189762fd9
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-09 21:42:30 -0400

    refactor: extract logic and use form action

--- app/src/routes/registration/+page.svelte
+++ app/src/routes/registration/+page.svelte
@@ -1,59 +1,8 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { pb } from '$lib/pb/pocketbase';
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
 
-	let username = '';
-	let email = '';
-	let password = '';
-	let passwordConfirm = '';
-	let errorMessage = '';
-	let successMessage = '';
-
-	async function register() {
-		errorMessage = '';
-		successMessage = '';
-
-		if (password != passwordConfirm) {
-			errorMessage = 'Passwords do not match!';
-			console.error('Passwords Dont Match for User!');
-			return;
-		}
-
-		try {
-			// I added new fields to the "users" colleciton
-			await pb.collection('users').create({
-				username,
-				email,
-				password,
-				passwordConfirm,
-			authToken: ''
-			});
-			successMessage = 'Account created!';
-			// Im assuming the login route will be /login, but CHANGE THIS if not
-			setTimeout(() => goto('/login'), 1500);
-		} catch (error: any) {
-			console.error('Something Went Wrong, Account Not Created', error);
-
-			if (error?.response?.data) {
-				let errors = error.response.data;
-
-				if (errors?.username) {
-					errorMessage = errorMessage + 'Username: ' + errors.username.message + ' ';
-				}
-
-				if (errors?.password) {
-					errorMessage = errorMessage + 'Password: ' + errors.password.message + ' ';
-				}
-
-				if (errors?.email) {
-					errorMessage = errorMessage + 'Email: ' + errors.email.message + ' ';
-				}
-
-			} else {
-				errorMessage = 'Something Went Wrong, No Account Created!';
-			}
-		}
-	}
+	export let form: ActionData;
 </script>
 
 <svelte:head>
@@ -62,11 +11,14 @@
 
 <div class="mx-auto max-w-md p-4">
 	<h1 class="mb-4 text-2xl font-bold">Registration</h1>
-
-	<form on:submit|preventDefault={register} class="flex flex-col gap-4">
+	{#if form?.error}
+		<p class="text-red-600">{form.error}</p>
+	{/if}
+	<form method="POST" use:enhance class="flex flex-col gap-4">
 		<input
 			type="text"
-			bind:value={username}
+			name="username"
+			value={form?.username ?? ''}
 			placeholder="Username"
 			required
 			autocomplete="off"
@@ -74,7 +26,8 @@
 		/>
 		<input
 			type="email"
-			bind:value={email}
+			name="email"
+			value={form?.email ?? ''}
 			placeholder="Email"
 			required
 			autocomplete="off"
@@ -82,7 +35,7 @@
 		/>
 		<input
 			type="password"
-			bind:value={password}
+			name="password"
 			placeholder="Password"
 			required
 			autocomplete="off"
@@ -90,7 +43,7 @@
 		/>
 		<input
 			type="password"
-			bind:value={passwordConfirm}
+			name="passwordConfirm"
 			placeholder="Confirm Password"
 			required
 			autocomplete="off"
@@ -101,12 +54,5 @@
 			class="rounded bg-indigo-600 px-6 py-3 font-bold text-white hover:bg-indigo-700"
 			style="cursor:pointer">Register</button
 		>
-
-		{#if errorMessage}
-			<p class="text-red-600">{errorMessage}</p>
-		{/if}
-		{#if successMessage}
-			<p class="text-green-600">{successMessage}</p>
-		{/if}
 	</form>
 </div>
