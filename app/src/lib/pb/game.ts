211b4f88bc7958fc363ebb0c448ab0eebcffddbf
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-14 17:11:13 -0400

    fix: format

--- app/src/lib/pb/game.ts
+++ app/src/lib/pb/game.ts
@@ -1,10 +1,10 @@
-import { pb } from "./pocketbase"
+import { pb } from './pocketbase';
 
 export type Position = {
-  entity_name: string
-  pos_x: number
-  pos_y: number
-}
+	entity_name: string;
+	pos_x: number;
+	pos_y: number;
+};
 
 export type PositionRecord = Position & {
 	id: string;
@@ -12,33 +12,36 @@ export type PositionRecord = Position & {
 	collectionName: string;
 	created: string;
 	updated: string;
-}
-  
-export async function updatePosition(p: Position){
-  try {
-	console.log("Updating position");
-	await pb.collection('positions').create(p);
-  	} catch (error) {
-	console.error('Failed to update position:', error);
-  	}
+};
+
+export async function updatePosition(p: Position) {
+	try {
+		console.log('Updating position');
+		await pb.collection('positions').create(p);
+	} catch (error) {
+		console.error('Failed to update position:', error);
+	}
 }
 
 export async function subscribeToPositions(
 	callback: (position: PositionRecord) => void
 ): Promise<() => void> {
 	try {
-		return await pb.collection('positions').subscribe('*', ({ action, record }) => {
-			if (action === 'create') {
-				console.log('Entity moved:', record);
-				callback(record as PositionRecord);
+		return await pb.collection('positions').subscribe(
+			'*',
+			({ action, record }) => {
+				if (action === 'create') {
+					console.log('Entity moved:', record);
+					callback(record as PositionRecord);
+				}
+			},
+			{
+				maxRetries: 3,
+				reconnectAttempts: 3
 			}
-		}, {
-		maxRetries: 3,
-		reconnectAttempts: 3,
-	  });
+		);
 	} catch (error) {
 		console.error('Failed to subscribe to position updates:', error);
 		return () => {}; // Return empty function if subscription fails
 	}
 }
-
