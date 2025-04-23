0428549742f3a75ceb4bc6bb1fcb26149f6f3d7e
Mahdi Ashrafee <mahdiashrafee@gmail.com>
2025-04-23 16:42:32 -0400

    leaderboard's functional

---
+++ pocketbase/pb_migrations/1745440400_updated_users.js
@@ -0,0 +1,22 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "authRule": "",
+    "listRule": null
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "authRule": null,
+    "listRule": ""
+  }, collection)
+
+  return app.save(collection)
+})
