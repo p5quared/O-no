4f99ea2f9899ae8fbf47e5746f7afd54a2d27ffc
mahdiashrafee <mahdiash@buffalo.edu>
2025-04-23 01:32:38 -0400

    feat: generate PB types

--- app/src/lib/pb/pocketbase.ts
+++ app/src/lib/pb/pocketbase.ts
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
+import type { TypedPocketBase } from './types/pocketbase';
 
 /**
  * Single PocketBase instance for the entire app
  */
-export const pb = new PocketBase('http://localhost:8090');
+export const pb = new PocketBase('http://localhost:8090') as TypedPocketBase;
