3d1da36a7912a26b2e13137517e093755830e85e
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-23 01:32:30 -0400

    feat: position subscriptions

---
+++ app/src/lib/pb/subscriptions/players.ts
@@ -0,0 +1,16 @@
+import type { GameID } from "$lib/constants";
+import type { EventSubscriptionInstance } from "$lib/events/Subscriptions";
+import { TABLES } from "../constants";
+import { pb } from "../pocketbase";
+import type { PlayerPositionsRecord, PlayerPositionsResponse } from "../types/pocketbase";
+
+type PositionHandler = (pr: PlayerPositionsRecord) => void;
+export const subscribeToPlayerPositions = (id: GameID, f: PositionHandler): EventSubscriptionInstance => {
+  pb.collection(TABLES.PLAYER_POSITIONS).subscribe<PlayerPositionsResponse>('update', (e) => {
+	f(e.record);
+  })
+
+  return {
+	cancel: () => pb.collection(TABLES.PLAYER_POSITIONS).unsubscribe('update')
+  }
+}
