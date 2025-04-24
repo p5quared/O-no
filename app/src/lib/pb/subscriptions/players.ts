a7128e25dc946673f427bd2960df0ae2be3c2bc0
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-23 22:12:14 -0400

    feat: Add subscriptions for player position updates, creates, and deletes

--- app/src/lib/pb/subscriptions/players.ts
+++ app/src/lib/pb/subscriptions/players.ts
@@ -1,17 +1,19 @@
-import type { GameID } from "$lib/constants";
-import type { EventSubscriptionInstance } from "$lib/events/Subscriptions";
 import { TABLES } from "../constants";
 import { pb } from "../pocketbase";
 import type { PlayerPositionsRecord, PlayerPositionsResponse } from "../types/pocketbase";
 
+type PocketbaseEvent = "create" | "update" | "delete";
 type PositionHandler = (pr: PlayerPositionsRecord) => void;
-export const subscribeToPlayerPositions = (id: GameID, f: PositionHandler): EventSubscriptionInstance => {
-  // TODO: Filter by game id when we add lobby support
-  pb.collection(TABLES.PLAYER_POSITIONS).subscribe<PlayerPositionsResponse>('update', (e) => {
-	f(e.record);
-  })
 
-  return {
-	cancel: () => pb.collection(TABLES.PLAYER_POSITIONS).unsubscribe('update')
-  }
+const subscribetoPlayerPositions = (e_T: PocketbaseEvent) => async (f: PositionHandler) => {
+	await pb.collection(TABLES.PLAYER_POSITIONS).subscribe<PlayerPositionsResponse>(e_T, (e) => {
+		f(e.record);
+	})
+	return {
+		cancel: () => pb.collection(TABLES.PLAYER_POSITIONS).unsubscribe(e_T)
+	}
 }
+
+export const subscribeToPlayerPositionUpdates = subscribetoPlayerPositions("update");
+export const subscribeToPlayerPositionCreates = subscribetoPlayerPositions("create");
+export const subscribeToPlayerPositionDeletes = subscribetoPlayerPositions("delete");
