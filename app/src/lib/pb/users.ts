0e35ffc724458560608cde4031b9ae7c9fdf11cf
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-24 00:03:55 -0400

    feat: various user positions table helpers

--- app/src/lib/pb/users.ts
+++ app/src/lib/pb/users.ts
@@ -1,4 +1,6 @@
+import { TABLES } from './constants';
 import { pb } from './pocketbase';
+import type { PlayerPositionsRecord } from './types/pocketbase';
 
 export interface UserCreateData {
 	username: string;
@@ -55,4 +57,50 @@ export async function createUser(userData: UserCreateData): Promise<UserCreateRe
 			error: errorMessage
 		};
 	}
+}
+
+export function getLoggedInUserID(): string {
+	return pb.authStore.record?.id ?? '';
+}
+
+export async function listUserPositions() {
+	return await pb.collection(TABLES.PLAYER_POSITIONS).getFullList();
+}
+
+// Creates a new user position record,
+// and possible deletes the old one.
+export async function createOrRecreateUserPositionRecord(userID: string, x: number, y: number): Promise<string> {
+	try {
+		await deleteUserPositionRecordByUserId(userID);
+	} catch (error) {
+		console.error('Error creating or recreating user position record:', error);
+	}
+
+
+	const { id } = await pb.collection(TABLES.PLAYER_POSITIONS).create({
+		user: userID,
+		x: x,
+		y: y
+	})
+
+	return id;
+}
+
+export async function deleteUserPositionRecordByUserId(userID: string) {
+  try {
+  		const { id } = await pb.collection(TABLES.PLAYER_POSITIONS).getFirstListItem<PlayerPositionsRecord>(`user="${userID}"`);
+  		if (id) {
+  			await pb.collection(TABLES.PLAYER_POSITIONS).delete(id);
+  		}
+  	} catch (error) {
+  		console.error('Error deleting user position record:', error);
+  	}
+  }
+
+export async function deleteUserPositionRecord(id: string) {
+  try {
+  await pb.collection(TABLES.PLAYER_POSITIONS).delete(id);
+  } catch (error) {
+	console.error('Error deleting user position record:', error);
+  }
 }
