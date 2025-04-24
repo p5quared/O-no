859095af8cfcf82b6ca14df3d51e80a32bfdb2fa
mahdiashrafee <mahdiash@buffalo.edu>
2025-04-24 00:03:33 -0400

    fix: player subscriptions

--- app/src/lib/pb/subscriptions/players.ts
+++ app/src/lib/pb/subscriptions/players.ts
@@ -2,18 +2,15 @@ import { TABLES } from "../constants";
 import { pb } from "../pocketbase";
 import type { PlayerPositionsRecord, PlayerPositionsResponse } from "../types/pocketbase";
 
-type PocketbaseEvent = "create" | "update" | "delete";
-type PositionHandler = (pr: PlayerPositionsRecord) => void;
+export type PbTableEvent = "create" | "update" | "delete";
+type PositionHandler = (action: PbTableEvent ,pr: PlayerPositionsRecord) => void;
 
-const subscribetoPlayerPositions = (e_T: PocketbaseEvent) => async (f: PositionHandler) => {
-	await pb.collection(TABLES.PLAYER_POSITIONS).subscribe<PlayerPositionsResponse>(e_T, (e) => {
-		f(e.record);
+export const subscribetoPlayerPositionTable = async (f: PositionHandler) => {
+	await pb.collection(TABLES.PLAYER_POSITIONS).subscribe<PlayerPositionsResponse>("*", (e) => {
+		f(e.action as PbTableEvent, e.record);
 	})
 	return {
-		cancel: () => pb.collection(TABLES.PLAYER_POSITIONS).unsubscribe(e_T)
+		cancel: () => pb.collection(TABLES.PLAYER_POSITIONS).unsubscribe("*")
 	}
 }
 
-export const subscribeToPlayerPositionUpdates = subscribetoPlayerPositions("update");
-export const subscribeToPlayerPositionCreates = subscribetoPlayerPositions("create");
-export const subscribeToPlayerPositionDeletes = subscribetoPlayerPositions("delete");
