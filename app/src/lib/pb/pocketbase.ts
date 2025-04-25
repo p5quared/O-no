9e9a04a4e4163dee831ee629950489376bbd0f5a
mahdiashrafee <mahdiash@buffalo.edu>
2025-04-25 19:39:28 +0000

    deployment_config

--- app/src/lib/pb/pocketbase.ts
+++ app/src/lib/pb/pocketbase.ts
@@ -1,7 +1,1 @@
-import PocketBase from 'pocketbase';
-import type { TypedPocketBase } from './types/pocketbase';
-
-/**
- * Single PocketBase instance for the entire app
- */
-export const pb = new PocketBase('http://localhost:8090') as TypedPocketBase;
+import PocketBase from 'pocketbase'; export const pb = new PocketBase('https://ono.myintro.link');
