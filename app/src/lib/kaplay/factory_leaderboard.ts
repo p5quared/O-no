445c8831782b33210db1a8ed43c70e21b9885d1a
mahdiashrafee <mahdiash@buffalo.edu>
2025-04-26 13:10:12 -0400

    Chat username fix (#40)
    (updating username fix with lobbies branch so I can properly implement "player.name" instead of "players.username" in lobbies, preventing conflicts later) 
    Co-authored-by: Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
    Co-authored-by: dsgulvin <dgulvin05@gmail.com>
    Co-authored-by: Mahdi Ashrafee <mahdiashrafee@gmail.com>
    Co-authored-by: Mahdi Ashrafee <mahdiash@buffalo.edu>

---
+++ app/src/lib/kaplay/factory_leaderboard.ts
@@ -0,0 +1,44 @@
+
+// TODO: Connect with events from Coduit (player_moved)
+// Updates the leaderboard based on player height, every 5 seconds
+//const updateLeaderboard = () => {
+//	const sorted = Object.entries(maxHeights)
+//		.sort(([, aY], [, bY]) => bY - aY) // Higher height = higher rank
+//		.slice(0, 10);
+//
+//
+//	const lines = sorted.map(([playerName, y], i) => {
+//		const heightDisplay = y < 100 ? "(0)" : `(${Math.round(y)}m)`;
+//		return `${i + 1}. ${playerName} ${heightDisplay}`;
+//	}).join("\n");
+//
+//	if (leaderboardText) leaderboardText.destroy();
+//	if (leaderboardBg) leaderboardBg.destroy();
+//
+//	leaderboardBg = add([
+//		rect(200, 170),
+//		pos(0, -400),
+//		color(10, 10, 10),
+//		z(100),
+//		layer("game"),
+//		opacity(0.8)
+//	]);
+//	add([
+//		rect(204, 174),
+//		pos(-2, -400),
+//		color(80, 80, 80),
+//		z(99),
+//		layer("game"),
+//		opacity(0.4)
+//	]);
+//
+//	leaderboardText = add([
+//		text(lines, { size: 12, lineSpacing: 4, font: "monospace" }),
+//		pos(10, 25), // shift down for title spacing
+//		z(101),
+//		layer("game")
+//	]);
+//};
+//
+//setInterval(updateLeaderboard, 5000);
+//
---
+++ pocketbase/pb_migrations/1745558575_updated_chat.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1116771610")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text4166911607",
+    "max": 0,
+    "min": 0,
+    "name": "name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1116771610")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text4166911607",
+    "max": 0,
+    "min": 0,
+    "name": "username",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
