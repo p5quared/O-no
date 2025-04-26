3dffd0b644b864f625970bbf6ab070ba8518b539
mahdiashrafee <mahdiash@buffalo.edu>
2025-04-26 11:50:03 -0400

    remove chat tab

--- app/src/routes/Header.svelte
+++ app/src/routes/Header.svelte
@@ -21,9 +21,6 @@
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about" style="font-family: 'FrogFont', sans-serif;">About</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/chat' ? 'page' : undefined}>
-				<a href="/chat" style="font-family: 'FrogFont', sans-serif;">Chat</a>
-			</li>
 
 			{#if pb.authStore.model}
 				<li>
