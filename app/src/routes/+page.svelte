c95e1414bd7ffe3c536f08d9e38f4669dfda6c66
AriaNas202 <anasca2022@gmail.com>
2025-04-27 14:35:32 -0400

    Made temporary homepage functionality

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -72,6 +72,24 @@
 		goto(`/lobby?id=${lobbyId}`);
 	}
 
+	function joinGame() {
+		goto('/demo-game');
+	}
+
+	async function resetGame() {
+		try {
+		const records = await pb.collection('positions').getFullList();
+		for (const record of records) {
+			await pb.collection('positions').delete(record.id);
+		}
+		//console.log('Game reset!');
+		goto('/demo-game');
+		} catch (error) {
+		console.error('Error resetting game:', error);
+		}
+	}
+
+
 </script>
 
 
@@ -82,9 +100,14 @@
 
 <section class="lobby-page" style="background-image: url({homepageBackground});">
 	<div class="mx-auto max-w-md p-4 form-box">
+		<h1 style="font-family: 'FrogFont', sans-serif; color: #212e1d;">Welcome, Frog!</h1>
+		<h2 style="font-family: 'FrogFont', sans-serif; color: #212e1d;">Click "Start" to Reset the Game!</h2>
+		<button class="create-btn" style="font-family: 'FrogFont', sans-serif; color: #212e1d;" on:click={resetGame}>Start</button>
+		<h2 style="font-family: 'FrogFont', sans-serif; color: #212e1d;">Click "Join" to Go to Existing Game!</h2>
+		<button class="create-btn" style="font-family: 'FrogFont', sans-serif; color: #212e1d;" on:click={joinGame}>Join</button>
 
 
-    
+<!-- REMOVED FOR TEMP LOBBY: 
         <h1 style="font-family: 'FrogFont', sans-serif;">Join a Lobby! </h1>
 
         <div class="lobby-list">
@@ -108,8 +131,8 @@
 			class="rounded"
 			style="color:black;"
 		/>
+-->		
 
-	
 	</div>
   
 </section>
