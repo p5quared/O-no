630ad086fbf3d413ad5c3ad10ff4f8ccd84d0c44
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-27 23:13:10 -0400

    feat: deploy pb to fly

--- app/src/lib/pb/pocketbase.ts
+++ app/src/lib/pb/pocketbase.ts
@@ -5,5 +5,5 @@ import type { TypedPocketBase } from './types/pocketbase';
  * Single PocketBase instance for the entire app
  */
 const LOCAL_PB_URL = import.meta.env.VITE_PB_URL;
-const PROD_PB_URL = "PROD_URL";
+const PROD_PB_URL = "https://pocketbase-twilight-surf-4702.fly.dev";
 export const pb = new PocketBase(LOCAL_PB_URL ?? PROD_PB_URL) as TypedPocketBase;
