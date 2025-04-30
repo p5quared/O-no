c2109c791ff32779b2b08aa560dc4cafbb798678
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-30 02:26:55 -0400

    init game log

---
+++ app/src/routes/(game)/demo-game/GameLog.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Conduit } from "$lib/events";
+	import { GameEventTypes } from "$lib/events/EventTypes";
+
+	const events = []
+Conduit.on(GameEventTypes.PLAYER_MOVED, e => events.push(e));
+</script>
+
+{#if events.length > 0}
+	<div class="flex-2/5">
+		{#each events as event}
+			<div class="event">
+				{JSON.stringify(event)}
+			</div>
+		{/each}
+	</div>
+{/if}
+
