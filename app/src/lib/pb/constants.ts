481f27b3912751d089d735258dc74b5958e327b0
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-29 21:47:45 -0400

    feat: create profile table

--- app/src/lib/pb/constants.ts
+++ app/src/lib/pb/constants.ts
@@ -2,4 +2,5 @@ export const TABLES = {
   PLAYER_POSITIONS: 'player_positions',
   GAME_EVENTS: 'events_games',
   USERS: 'users',
+  PROFILES: 'profiles',
 }
---
+++ pocketbase/pb_migrations/1745977577_created_profiles.js
@@ -0,0 +1,86 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": "",
+    "deleteRule": "",
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "cascadeDelete": true,
+        "collectionId": "_pb_users_auth_",
+        "hidden": false,
+        "id": "relation2375276105",
+        "maxSelect": 1,
+        "minSelect": 0,
+        "name": "user",
+        "presentable": false,
+        "required": true,
+        "system": false,
+        "type": "relation"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text891129758",
+        "max": 0,
+        "min": 0,
+        "name": "sprite",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "autodate2990389176",
+        "name": "created",
+        "onCreate": true,
+        "onUpdate": false,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      },
+      {
+        "hidden": false,
+        "id": "autodate3332085495",
+        "name": "updated",
+        "onCreate": true,
+        "onUpdate": true,
+        "presentable": false,
+        "system": false,
+        "type": "autodate"
+      }
+    ],
+    "id": "pbc_3414089001",
+    "indexes": [
+      "CREATE INDEX `idx_lKDSSV5mxQ` ON `profiles` (`user`)"
+    ],
+    "listRule": "",
+    "name": "profiles",
+    "system": false,
+    "type": "base",
+    "updateRule": "",
+    "viewRule": ""
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3414089001");
+
+  return app.delete(collection);
+})
