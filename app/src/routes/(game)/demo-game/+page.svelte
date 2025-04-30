6443a8cc6c1e3189f317c002c0f81f5a62b93058
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2025-04-30 02:28:27 -0400

    feat: add GameLog component to demo game page layout

--- app/src/routes/(game)/demo-game/+page.svelte
+++ app/src/routes/(game)/demo-game/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import { init } from '$lib/kaplay/game';
 	import { onMount } from 'svelte';
+	import GameLog from './GameLog.svelte';
 
 	onMount(async () => {
 		await init();
 	});
 
 </script>
+
+<div class="min-h-full flex">
+  <canvas id="game-canvas "/>
+  <GameLog />
+</div>
